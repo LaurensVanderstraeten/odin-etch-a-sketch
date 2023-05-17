677df875a4323b568a7f5396dd1e1918bdd20b63
Laurens Vanderstraeten <laurensvanderstraeten@gmail.com>
2023-05-17 16:50:16 -0400

    Make Grid Row and Square classes

--- javascript.js
+++ javascript.js
@@ -1,1 +1,66 @@
+const NUMBER_OF_ROWS = 16;
+const NUMBER_OF_COLUMNS = 16;
 const container = document.querySelector('#container');
+let grid;
+
+class Grid {
+
+    #rows;
+    #htmlElement;
+
+    constructor(htmlElement, numberOfRows, numberOfColumns) {
+        this.#rows = new Array(numberOfRows);
+        this.#htmlElement = this.createHTMLElement();
+        for (let rowNumber = 1; rowNumber <= numberOfRows; rowNumber++) {
+            this.#rows.push(new Row(this, numberOfColumns));
+        }
+    }
+
+    createHTMLElement(){
+
+    }
+
+}
+
+class Row {
+
+    #parentGrid
+    #squares
+    #htmlElement
+
+    constructor(parentGrid, numberOfColumns) {
+        this.parentGrid = parentGrid;
+        this.#squares = new Array(numberOfColumns);
+        this.#htmlElement = this.createHTMLElement();
+        for (let columnNumber = 1; columnNumber <= numberOfColumns; columnNumber++) {
+            this.#squares.push(new Square(this));
+        }
+    }
+
+    createHTMLElement(){
+
+    }
+    
+}
+
+class Square {
+
+    #parentRow
+    #htmlElement
+
+    constructor(parentRow) {
+        this.#parentRow = parentRow;
+        this.#htmlElement = this.createHTMLElement();
+    }
+
+    createHTMLElement(){
+
+    }
+
+}
+
+main();
+
+function main() {
+    grid = new Grid(container, NUMBER_OF_ROWS, NUMBER_OF_COLUMNS);
+}
